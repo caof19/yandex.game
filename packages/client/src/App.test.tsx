0c2a31ebd02df4831e9c345b5c2bcea2299a89e4
Pavel <alexpavel35@yandex.ru>
2024-09-21 01:58:51 +0300

    Исключили jest из eslint

--- packages/client/src/App.test.tsx
+++ packages/client/src/App.test.tsx
@@ -3,6 +3,8 @@ import { render, screen } from "@testing-library/react";
 
 const appContent = "Вот тут будет жить ваше приложение :)";
 
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+// @ts-ignore
 global.fetch = jest.fn(() =>
     Promise.resolve({ json: () => Promise.resolve("hey") }),
 );
