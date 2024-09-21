f6376ed1e458f513ca8d3d2ad452097ddee519ba
Pavel <alexpavel35@yandex.ru>
2024-09-21 21:44:31 +0300

    Исправили опечатки

--- packages/client/src/service/routes/AuthGuard.tsx
+++ packages/client/src/service/routes/AuthGuard.tsx
@@ -5,9 +5,9 @@ import { routes } from "./routeMap";
 export const AuthGuard: React.FC<PropsWithChildren<any>> = ({ children }) => {
     const authentificate = true;
 
-    if (authentificate) {
-        return children;
-    } else {
-        <Redirect to={routes.signIn.path} />;
+    if (!authentificate) {
+        return <Redirect to={routes.signIn.path} />;
     }
+
+    return children;
 };
