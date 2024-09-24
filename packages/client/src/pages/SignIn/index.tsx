e48db90129bdd6f881a87c7266288467028f9ef6
caof19 <caof19@mail.ru>
2024-09-24 23:15:40 +0300

    Add front for sign in

--- packages/client/src/pages/SignIn/index.tsx
+++ packages/client/src/pages/SignIn/index.tsx
@@ -1,5 +1,44 @@
 import React from "react";
+import { Form, Input, Button } from "antd";
+import "./style.css";
 
-export const SingIn: React.FC<object> = () => {
-    return <div>SingIn:</div>;
+const userFields = [
+    {
+        name: "login",
+        placeholder: "Введите ваш логин",
+        type: "text",
+    },
+    {
+        name: "password",
+        placeholder: "Введите пароль",
+        type: "password",
+    },
+];
+
+export const SingIn: React.FC = () => {
+    return (
+        <div className="form-container">
+            <Form className="custom-form">
+                {userFields.map((field) => (
+                    <Form.Item key={field.name} name={field.name}>
+                        <Input
+                            type={field.type}
+                            placeholder={field.placeholder}
+                            className="custom-input"
+                        />
+                    </Form.Item>
+                ))}
+
+                <Form.Item>
+                    <Button
+                        type="primary"
+                        htmlType="submit"
+                        className="custom-button"
+                    >
+                        Зарегистрироваться
+                    </Button>
+                </Form.Item>
+            </Form>
+        </div>
+    );
 };
