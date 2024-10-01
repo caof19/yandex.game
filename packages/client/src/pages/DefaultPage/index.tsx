ed80caeea5b697446df3db86e53e28d56f85a218
caof19 <caof19@mail.ru>
2024-10-01 23:42:05 +0300

    5 // Add front to 404 and 50x pages

--- packages/client/src/pages/DefaultPage/index.tsx
+++ packages/client/src/pages/DefaultPage/index.tsx
@@ -1,6 +1,21 @@
-import { Redirect } from "react-router-dom";
-import { routes } from "../../service";
+import { useHistory } from "react-router-dom";
+import style from "./DefaultPage.module.css";
 
 export const DefaultPage = () => {
-    return <Redirect to={routes.signIn.path} />;
+    const history = useHistory();
+
+    return (
+        <div className={style.wrapper}>
+            <span className={style.big}>404</span>
+
+            <button
+                className={style.button}
+                onClick={() => {
+                    history.push("/auth/sign-in"); // Укажите путь к целевой странице
+                }}
+            >
+                Вернуться на главную
+            </button>
+        </div>
+    );
 };
--- packages/client/src/pages/ServerError/index.tsx
+++ packages/client/src/pages/ServerError/index.tsx
@@ -1,5 +1,12 @@
-import React from "react";
+import { useHistory } from "react-router-dom";
+import style from "./ServerError.module.css";
 
-export const ServerError: React.FC<object> = () => {
-    return <div>ServerError:</div>;
+export const ServerError = () => {
+    const history = useHistory();
+
+    return (
+        <div className={style.wrapper}>
+            <span className={style.big}>500</span>
+        </div>
+    );
 };
