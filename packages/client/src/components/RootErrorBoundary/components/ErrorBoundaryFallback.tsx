ba3d2a2a986dc4097c21abc0a99abc2271215d57
ilya_rogachev <ilarogacev@ilya-rogachev.local>
2024-10-07 20:36:37 +0500

    Исправил комментарии в ревью

--- packages/client/src/components/RootErrorBoundary/components/ErrorBoundaryFallback.tsx
+++ packages/client/src/components/RootErrorBoundary/components/ErrorBoundaryFallback.tsx
@@ -4,64 +4,32 @@ import { AxiosError } from "axios";
 import { FallbackProps } from "react-error-boundary";
 
 type ErrorBoundaryFallbackProps = ResultProps & FallbackProps;
+const STATUSES: Record<string, ResultStatusType> = {
+    403: "403",
+    404: "404",
+    500: "500",
+};
+const getStatusError = (status?: keyof typeof STATUSES) => {
+    if (!status) return STATUSES["500"];
+
+    return STATUSES[status] ?? STATUSES["500"];
+};
+const title = "Простите, что-то пошло не так...";
+const subTitle = "Попробуйте обновить страницу или повторите позже";
 
 export function ErrorBoundaryFallback({
     children,
     error,
     ...other
 }: ErrorBoundaryFallbackProps) {
-    if (error instanceof AxiosError) {
-        if (error.response) {
-            const axiosStatus = `${error.response.status}`;
-            const statuses: Record<string, ResultStatusType> = {
-                403: "403",
-                404: "404",
-                500: "500",
-            };
-            const status = statuses[axiosStatus] ?? "500";
-
-            return (
-                <Result
-                    status={status}
-                    title="Простите, что-то пошло не так..."
-                    subTitle="Попробуйте обновить страницу или повторите позже"
-                    {...other}
-                >
-                    {children}
-                </Result>
-            );
-        } else if (error.request) {
-            return (
-                <Result
-                    status="500"
-                    title="Простите, что-то пошло не так..."
-                    subTitle="Попробуйте обновить страницу или повторите позже"
-                    {...other}
-                >
-                    {children}
-                </Result>
-            );
-        } else {
-            return (
-                <Result
-                    status="error"
-                    title="Простите, что-то пошло не так..."
-                    subTitle="Попробуйте обновить страницу или повторите позже"
-                    {...other}
-                >
-                    {children}
-                </Result>
-            );
-        }
-    }
+    const isAxiosError = error instanceof AxiosError;
+    const status =
+        isAxiosError && error.response
+            ? getStatusError(`${error.response.status}`)
+            : "error";
 
     return (
-        <Result
-            status="error"
-            title="Простите, что-то пошло не так..."
-            subTitle="Попробуйте обновить страницу или повторите позже"
-            {...other}
-        >
+        <Result status={status} title={title} subTitle={subTitle} {...other}>
             {children}
         </Result>
     );
