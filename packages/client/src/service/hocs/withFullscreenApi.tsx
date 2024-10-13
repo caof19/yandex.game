e650fe127db7411e2f873555a56c03454fbde78b
shuraleva <tm.shuraleva@gmail.com>
2024-10-13 22:02:17 +0300

    fix game components

--- packages/client/src/service/hocs/withFullscreenApi.tsx
+++ packages/client/src/service/hocs/withFullscreenApi.tsx
@@ -2,8 +2,8 @@ import { FullscreenExitOutlined, FullscreenOutlined } from "@ant-design/icons";
 import { Flex, FloatButton } from "antd";
 import { FC, useRef, useState } from "react";
 
-export const withFullscreenApi = (WrappedBlock: FC) => {
-    const Wrapper = () => {
+export const withFullscreenApi = <T extends object>(WrappedBlock: FC<T>) => {
+    const Wrapper = (props: T) => {
         const [isFullscreenActive, setFullscreen] = useState(false);
         const fullscreenRef = useRef<HTMLDivElement>(null);
         const handleClick = () => {
@@ -21,7 +21,7 @@ export const withFullscreenApi = (WrappedBlock: FC) => {
                 ref={fullscreenRef}
                 style={{ backgroundColor: "#ffffff" }}
             >
-                <WrappedBlock />
+                <WrappedBlock {...props} />
                 <FloatButton
                     onClick={handleClick}
                     icon={
